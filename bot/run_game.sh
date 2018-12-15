c1b5ca4c9e5c49d66ea3c0a761c4e9716d429f1d
Tyler Horth <tylerhorth@outlook.com>
2018-12-14 21:57:48 -0800

    add size variable

--- bot/run_game.sh
+++ bot/run_game.sh
@@ -2,15 +2,17 @@
 
 set -e
 
+[ -z "$SIZE" ] && SIZE=32
+
 if [ "$1" == "-d" ]; then
   cargo build
-  ./halite --replay-directory replays/ -vvv --width 32 --height 32 "RUST_BACKTRACE=1 ./target/debug/my_bot" "RUST_BACKTRACE=1 ./target/debug/my_bot"
+  ./halite --replay-directory replays/ -vvv --width $SIZE --height $SIZE "RUST_BACKTRACE=1 ./target/debug/my_bot" "RUST_BACKTRACE=1 ./target/debug/my_bot"
 elif [ "$1" == "-o" ]; then
   cargo build --release
-  ./halite --replay-directory replays/ -vvv --width 32 --height 32 "./target/release/my_bot" "./$2"
+  ./halite --replay-directory replays/ -vvv --width $SIZE --height $SIZE "./target/release/my_bot" "./$2"
 else
   cargo build --release
-  ./halite --replay-directory replays/ -vvv --width 32 --height 32 "./target/release/my_bot" "./target/release/my_bot"
+  ./halite --replay-directory replays/ -vvv --width $SIZE --height $SIZE "./target/release/my_bot" "./target/release/my_bot"
 fi
 
 cat bot-0.log >bot.log
